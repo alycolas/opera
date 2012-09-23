5360271adb0469804eb913b2c3b7e5a417e7107c
alycolas <alycolas@gmail.com>
2012-09-23 08:41:33 +0800

    win8

--- mplayer.sh
+++ mplayer.sh
@@ -2,11 +2,42 @@
 # Latest Version 2010.07.22
 # See ChangeLog 2008-09-12
 
-flv_web_addr=$(echo "$1" | sed 's/^http:\/\([a-z0-9]\)/http:\/\/\1/')
-
-wget -O /tmp/flv-search-result.txt "http://www.flvcd.com/parse.php?kw=$1&format=super"
-
-grep "<U>" /tmp/flv-search-result.txt | sed s/\<U\>//g | mplayer -playlist -
+if [ "$#" != 1 ]
+then
+	PLAYER='mplayer'
+	QULITY='super'
+	while getopts ":svdnh:" PAR
+	do 
+		case $PAR in
+			s)
+				QULITY='super'
+				;;
+			h)
+				QULITY='high'
+				;;
+			n)
+				QULITY='normal'
+				;;
+			v)
+				PLAYER='cvlc'
+				;;
+			d)
+				PLAYER='wget'
+				;;
+		esac
+	done
+	# echo $PAR $PLAYER $QULITY
+	if [ "$#" = 2 ]
+	then
+		# echo $PAR $PLAYER $QULITY
+		wget -O - "http://www.flvcd.com/parse.php?kw=$2&format=$QULITY" | grep "<U>" - | sed s/\<U\>//g |xargs $PLAYER
+	else 
+		# echo $PAR $PLAYER $QULITY
+		wget -O - "http://www.flvcd.com/parse.php?kw=$3&format=$QULITY" | grep "<U>" - | sed s/\<U\>//g |xargs $PLAYER 
+	fi
+else 
+	wget -O - "http://www.flvcd.com/parse.php?kw=$1&format=super" | grep "<U>" - | sed s/\<U\>//g |xargs mplayer -
+fi
+# grep "<U>" /tmp/flv-search-result.txt | sed s/\<U\>//g | mplayer -playlist -
 # grep "<U>" /tmp/flv-search-result.txt | sed s/\<U\>//g | vlc -
-
-rm -f /tmp/flv-search-result.txt
+# rm -f /tmp/flv-search-result.txt
